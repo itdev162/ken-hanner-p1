9e9aa59adabf085a0f739c0cdebb4ce0588e3554
ken <hannerk@gmatc.matc.edu>
2021-10-25 00:00:38 -0500

    Final P1 Complete

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -25,11 +25,10 @@ namespace TodoApi
 
             services.AddDbContext<TodoContext>(opt =>
                                                 opt.UseInMemoryDatabase("TodoList"));
-
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
-            // });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApi", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,8 +37,8 @@ namespace TodoApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                // app.UseSwagger();
-                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
             }
 
             app.UseHttpsRedirection();
